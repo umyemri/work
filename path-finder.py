f0f4c70153ae1c590cbecd6acce0a3418461f96e
umyemri <40582475+umyemri@users.noreply.github.com>
2018-07-03 10:59:58 -0700

    Update path-finder.py
    better unicode handling

--- path-finder.py
+++ path-finder.py
@@ -1,21 +1,28 @@
 import os, io, time
 
 start_time = time.time()
-startpath = "U:\\"
-content   = ""
+startpath  = 'C:\\Users\\andrews\\'
+newfile    = 'C:\\Users\\andrews\\Local\\paths.txt'
+content    = ''
+totaldirs  = 0
+totalfiles = 0
 
-with io.open ("C:\\Users\\andrews\\Local\\paths-U.txt", "a", encoding="utf-8") as myfile:
-    print("processing...")
+with io.open (newfile, 'a', encoding='utf-8') as myfile:
+
+    print('processing...')
+
     for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = '\t' * (level)
-        content = content + '{}{}/'.format(indent, os.path.basename(root)) + '\n'
-        subindent = '\t' * (level + 1)
+        level     = root.replace(startpath, '').count(os.sep)
+        indent    = u'\t' * (level)
+        content   = content + u'{}{}/'.format(indent, os.path.basename(root)) + '\n'
+        subindent = u'\t' * (level + 1)
+        
         for f in files:
-            content = content + '{}{}'.format(subindent, f) + '\n'
+            content = content + u'{}{}'.format(subindent, f) + u'\n'
+
     myfile.write(content)
     myfile.close()
-    print("complete!")
+    print('complete!')
     elapsed_time = time.time() - start_time
     print(elapsed_time)
 
