94d98504630e9575670de470a897f7142885432c
umyemri <40582475+umyemri@users.noreply.github.com>
2018-07-24 14:13:19 -0700

    Create countdown_workday.py

---
+++ tools/py/countdown_workday.py
@@ -0,0 +1,77 @@
+## countdown_workday.py
+#
+# i had some free time... not really, i should have been working on
+# census data isolation. there are probably better ways of doing
+# this...
+#
+import time, os, math, progressbar
+import progressbar
+
+def nowish():
+	return time.localtime()
+
+def thour( et_hr ):
+	if ( nowish().tm_hour != et_hr ):
+		return False
+	else:
+		return True
+
+def tmin( et_min ):
+	if ( nowish().tm_min != et_min ):
+		return False
+	else:
+		return True
+
+def main():
+	daystart 	= float(input('Start of the day (e.g. 8.75 = 8:45 AM): '))
+	ds_hr 		= math.floor(daystart)
+	ds_min 		= round(abs(ds_hr - daystart) * 60)
+	shiftlength = 8.5
+	sl_hr 		= math.floor(shiftlength)
+	sl_min 		= round(abs(sl_hr - shiftlength) * 60)
+	endshift 	= daystart + shiftlength
+	et_hr 		= math.floor(endshift)
+	et_min 		= round(abs(et_hr - endshift) * 60)
+
+	interval 	= round( (shiftlength * 60) * 60 )
+	
+	cur_day		= nowish().tm_mday
+	cur_mon		= nowish().tm_mon
+	cur_year	= nowish().tm_year
+	cur_hour 	= nowish().tm_hour
+	cur_min 	= nowish().tm_min
+	cur_sec 	= nowish().tm_sec
+	current_pt	= '{}.{}.{} {}:{}:{}'.format(cur_day, cur_mon, cur_year, cur_hour, cur_min, cur_sec)
+	start_pt	= '{}.{}.{} {}:{}:00'.format(cur_day, cur_mon, cur_year, ds_hr, ds_min)
+	end_pt		= '{}.{}.{} {}:{}:00'.format(cur_day, cur_mon, cur_year, et_hr, et_min)
+	pattern 	= '%d.%m.%Y %H:%M:%S'
+	current_ep 	= int(time.mktime(time.strptime(current_pt, pattern)))
+	start_ep 	= int(time.mktime(time.strptime(start_pt, pattern)))
+	end_ep 		= int(time.mktime(time.strptime(end_pt, pattern)))
+	print('c: {}\ns: {}\ne: {}'.format(current_ep, start_ep, end_ep) )
+	print('c - s: {}'.format( abs(current_ep - start_ep) ) )
+	print('c - e: {}'.format( abs(current_ep - end_ep) ) )
+	bar = progressbar.ProgressBar(term_width=31, maxval=interval, \
+		widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+	os.system('cls')
+	
+	for i in range( abs(start_ep - current_ep), interval ):
+		bar.start()
+		bar.update(i + 1)
+		cur_hour 	= nowish().tm_hour
+		cur_min 	= nowish().tm_min
+		cur_sec 	= nowish().tm_sec
+		lf_hour 	= abs( et_hr - nowish().tm_hour )
+		lf_min		= abs( et_min - nowish().tm_min )
+		print()
+		print( 'c: {:02d}:{:02d}:{:02d}, '.format(cur_hour, cur_min, cur_sec), end='' )
+		print( 'g: {:02d}:{:02d}, '.format(et_hr, et_min), end='' )
+		print( 'l: {:02d}:{:02d}'.format(lf_hour, lf_min) )
+
+		time.sleep(1)
+		os.system('cls')
+	
+	bar.finish()
+	print("Done!")
+
+main()
