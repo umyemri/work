1816877041e626fa79e783e81d09a673ca15ad93
umyemri <40582475+umyemri@users.noreply.github.com>
2018-12-18 10:35:03 -0800

    Update census_state_agg.sql

--- tools/sql/census_state_agg.sql
+++ tools/sql/census_state_agg.sql
@@ -10,13 +10,13 @@
  */;
 
 -- tier 0: all us population table
-select sum( p0010001 ) as tot_us      -- total population in us
+select sum( p0010001 ) as tot_us          -- total population in us
 	 , sum( p0020007 ) as tot_us_aian -- total population in us for ai/an
 	 , ( cast( sum(p0020007) as float(8) ) / sum(p0010001) ) * 100 as aian_pct
-									  -- percentage from the total
+					  -- percentage from the total
 	 , sum( p0020005 ) as tot_us_wnh  -- total population in us for white non-hisp
 	 , ( cast( sum(p0020005) as float(8) ) / sum(p0010001) ) * 100 as wnh_pct
-									  -- percentage from the total
+					  -- percentage from the total
 from  [dbo].[census_counties_2010];
 
 -- tier 1: per state population table
@@ -25,10 +25,10 @@ select state_us_abbreviation as st_abbr
 	 , sum( p0010001 ) as tot_pop   -- total population in a state
 	 , sum( p0020007 ) as aian_pop  -- total population of ai/an in that state
 	 , ( cast( sum(p0020007) as float(8) ) / sum(p0010001) ) * 100 as aian_pct
-									-- ai/an percentage from the total
+					-- ai/an percentage from the total
 	 , sum( p0020005 ) as wnh_pop   -- total population of white non-hispanic in that states
 	 , ( cast( sum(p0020005) as float(8) ) / sum(p0010001) ) * 100 as wnh_pct
-									-- wnh percentage from the total
+					-- wnh percentage from the total
 from [dbo].[census_counties_2010]
 group by state_us_abbreviation
 order by st_fips;
